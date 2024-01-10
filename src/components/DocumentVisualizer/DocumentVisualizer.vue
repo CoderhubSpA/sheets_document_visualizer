f08d2783c845f41a3304387558e41da13f1c27b8
Jorge Peraza <japr8985@gmail.com>
2024-01-10 10:54:08 -0400

    Formatting

--- src/components/DocumentVisualizer/DocumentVisualizer.vue
+++ src/components/DocumentVisualizer/DocumentVisualizer.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <component :is="viewer" :blob="blob" :format="format" :dataEndpoint="dataEndpoint"/>
-    </div>
+  <div>
+    <component :is="viewer" :blob="blob" :format="format" :dataEndpoint="dataEndpoint" />
+  </div>
 </template>
 <script>
 
@@ -15,166 +15,165 @@ import CsvVisualizer from "@/components/CsvVisualizer/CsvVisualizer";
 import ErrorComponent from "@/components/ErrorComponent";
 import ImageVisualizer from "@/components/ImageVisualizer";
 import UnsupportedFormat from "@/components/UnsupportedFormat/UnsupportedFormat";
-import {Formats} from "@/helpers/Formats";
+import { Formats } from "@/helpers/Formats";
 import PptxVisualizer from "@/components/PptxVisualizer";
 
 export default {
-    name: 'document-visualizer',
-    props: {
-        /**
-         * URL del file a visualizar
-         */
-        src: {
-            type: [String, Blob],
-            required: true,
-        },
+  name: 'document-visualizer',
+  props: {
+    /**
+     * URL del archivo a visualizar
+     * o el File de un input[file]
+     */
+    src: {
+      type: [String, File],
+      required: true,
     },
-    data: () => ({
-        /**
-         * Formato del archivo obtenido
-         * @format String
-         */
-        format: '',
-        /**
-         * Representacion Blob del archivo
-         * @format Blob
-         */
-        blob: null
-    }),
-    computed: {
-        /**
-         * Importa el componente adecuado, para la visualizacion
-         * del file segun su formato
-         * @return component
-         */
-        viewer() {
-            let component;
-            switch (this.format) {
-                // text y csv
-                case 'text/plain':
-                    component = TextVisualizer; //() => import('../TextVisualizer');
-                    break;
-                // pdf
-                case 'application/pdf':
-                    component = PdfVisualizer; // () => import('../PdfVisualizer');
-                    break;
-                // docx
-                case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-                    component = DocxVisualizer; //() => import('../DocxVisualizer');
-                    break;
-                case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-                    component = XlsxVisualizer; // () => import('../XlsxVisualizer');
-                    break;
-              case 'text/csv':
-              case 'application/csv':
-                component = CsvVisualizer;
-                  break;
-              case 'error':
-                  component = ErrorComponent;
-                  break;
-              case 'image/png':
-              case 'image/jpeg':
-              case 'image/gif':
-              case 'image/svg+xml':
-                  component = ImageVisualizer;
-                break;
-              case 'application/vnd.ms-powerpoint':
-              case 'application/mspowerpoint':
-              case 'application/powerpoint':
-              case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
-                component = PptxVisualizer;
-                break;
-              case 'unsupported':
-                component = UnsupportedFormat;
-                break;
-              default:
-                  component = LoadingDocument; //() => import('../LoadingDocument');
-                  break;
-            }
-            return component;
-        },
-        /**
-         * Determina si la fuente entregada es de tipo String
-         * o no
-         * @return {String|null}
-         */
-        url() {
-          return this.src instanceof String ? this.src : null;
-        },
-        /**
-         * Construye el endpoint para la descarga del archivo
-         */
-        dataEndpoint() {
-          return this.url ? `/entity/data${this.src}` : null;
-        }
+  },
+  data: () => ({
+    /**
+     * Formato del archivo obtenido
+     * @format String
+     */
+    format: '',
+    /**
+     * Representacion Blob del archivo
+     * @format Blob
+     */
+    blob: null
+  }),
+  computed: {
+    /**
+     * Importa el componente adecuado, para la visualizacion
+     * del file segun su formato
+     * @return component
+     */
+    viewer() {
+      let component;
+      switch (this.format) {
+        // text y csv
+        case 'text/plain':
+          component = TextVisualizer; //() => import('../TextVisualizer');
+          break;
+        // pdf
+        case 'application/pdf':
+          component = PdfVisualizer; // () => import('../PdfVisualizer');
+          break;
+        // docx
+        case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+          component = DocxVisualizer; //() => import('../DocxVisualizer');
+          break;
+        case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+          component = XlsxVisualizer; // () => import('../XlsxVisualizer');
+          break;
+        case 'text/csv':
+        case 'application/csv':
+          component = CsvVisualizer;
+          break;
+        case 'error':
+          component = ErrorComponent;
+          break;
+        case 'image/png':
+        case 'image/jpeg':
+        case 'image/gif':
+        case 'image/svg+xml':
+          component = ImageVisualizer;
+          break;
+        case 'application/vnd.ms-powerpoint':
+        case 'application/mspowerpoint':
+        case 'application/powerpoint':
+        case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+          component = PptxVisualizer;
+          break;
+        case 'unsupported':
+          component = UnsupportedFormat;
+          break;
+        default:
+          component = LoadingDocument; //() => import('../LoadingDocument');
+          break;
+      }
+      return component;
     },
-    watch: {
-      src(value) {
-        this.readSrc(value)
+    /**
+     * Determina si la fuente entregada es de tipo String
+     * o no
+     * @return {String|null}
+     */
+    url() {
+      return this.src instanceof String ? this.src : null;
+    },
+    /**
+     * Construye el endpoint para la descarga del archivo
+     */
+    dataEndpoint() {
+      return this.url ? `/entity/data${this.src}` : null;
+    }
+  },
+  watch: {
+    src(value) {
+      this.readSrc(value)
+    }
+  },
+  /**
+   * Una vez creado el componente
+   * procede a realizar la solicitud al endpoint
+   * indicado
+   */
+  async created() {
+    this.readSrc()
+  },
+  methods: {
+    /**
+     * Si el visualizador posee una URL
+     * realiza un llamado al backend
+     * o lee desde un archivo cargado desde un input
+     * @return {Void}
+     */
+    readSrc() {
+      if (this.url) {
+        this.readFromURL()
+      } else {
+        this.readFromFile()
       }
     },
     /**
-     * Una vez creado el componente
-     * procede a realizar la solicitud al endpoint
-     * indicado
+     * Realiza un llamado al backend
+     * esperando que el tipo de respuesta sea manejada
+     * como un blob
+     * @return {Void}
      */
-    async created() {
-      this.readSrc()
-    },
-    methods: {
-      /**
-       * Si el visualizador posee una URL
-       * realiza un llamado al backend
-       * o lee desde un archivo cargado desde un input
-       * @return {Void}
-       */
-      readSrc() {
-        if (this.url) {
-          this.readFromURL()
+    readFromURL() {
+      axios.get(this.url, {
+        responseType: 'blob',
+      }).then((response) => {
+        if (Formats.isSupported(response.data.type)) {
+          this.format = response.data.type;
+          this.blob = new Blob([response.data]);
         } else {
-          this.readFromFile()
+          this.format = 'unsupported';
+          const data = {
+            src: this.src
+          }
+          this.blob = new Blob([JSON.stringify(data)]);
         }
-      },
-      /**
-       * Realiza un llamado al backend
-       * esperando que el tipo de respuesta sea manejada
-       * como un blob
-       * @return {Void}
-       */
-      readFromURL() {
-        axios.get(this.url, {
-            responseType: 'blob',
-        }).then((response) => {
-            if (Formats.isSupported(response.data.type)) {
-              this.format = response.data.type;
-              this.blob = new Blob([response.data]);
-            } else {
-              this.format = 'unsupported';
-              const data = {
-                src: this.src
-              }
-              this.blob =  new Blob([JSON.stringify(data)]);
-            }
 
-        }).catch((error) => {
-          console.log(error)
-            this.format = 'error';
-            this.blob = error.response;
-        });
-      },
-      /**
-       * Cuando el input del visualizador
-       * es un Archivo no cargado a la plataforma
-       * recibe un {File} y este se encarga de transformarlo como blob
-       * @return {Void}
-       */
-      readFromFile() {
-        this.format = this.src.type;
-        this.blob = new Blob([this.src], {type: this.format});
-      }
+      }).catch((error) => {
+        console.log(error)
+        this.format = 'error';
+        this.blob = error.response;
+      });
     },
+    /**
+     * Cuando el input del visualizador
+     * es un Archivo no cargado a la plataforma
+     * recibe un {File} y este se encarga de transformarlo como blob
+     * @return {Void}
+     */
+    readFromFile() {
+      this.format = this.src.type;
+      this.blob = new Blob([this.src], { type: this.format });
+    }
+  },
 };
 </script>
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
